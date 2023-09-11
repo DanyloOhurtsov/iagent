90e3a8452362c805ca5473199ae9d30dfb72769e
DanyloOhurtsov <118742794+DanyloOhurtsov@users.noreply.github.com>
2023-09-11 15:51:53 +0200

    Minor changes
    Added a check for property comments availability.

--- src/components/AllPages/NewsPage/CommentsComp.jsx
+++ src/components/AllPages/NewsPage/CommentsComp.jsx
@@ -40,46 +40,70 @@ export const CommentsComp = ({ value: { dataText } }) => {
                     <div className={styles.commentItem} key={item.id}>
                         <Card value={{ item, optionsForCard }} key={item.id} />
                         <div className={styles.commentDetails}>
-                            {item.dataComments.slice(0, 2).map((com) => (
-                                <div className={styles.comment} key={com.id}>
-                                    <div className={styles.commentAuthor}>
-                                        <div className={styles.authorPhoto}>
-                                            <img src={com.personPhoto} alt="" />
+                            {item.dataComments.length > 0 ? (
+                                item.dataComments.slice(0, 2).map((com) => (
+                                    <div
+                                        className={styles.comment}
+                                        key={com.id}
+                                    >
+                                        <div className={styles.commentAuthor}>
+                                            <div className={styles.authorPhoto}>
+                                                <img
+                                                    src={com.personPhoto}
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className={styles.authorInfo}>
+                                                <p
+                                                    className={
+                                                        styles.authorName
+                                                    }
+                                                >
+                                                    {com.personName}
+                                                </p>
+                                                <p
+                                                    className={
+                                                        styles.commentDate
+                                                    }
+                                                >
+                                                    {com.date}
+                                                </p>
+                                            </div>
                                         </div>
-                                        <div className={styles.authorInfo}>
-                                            <p className={styles.authorName}>
-                                                {com.personName}
-                                            </p>
-                                            <p className={styles.commentDate}>
-                                                {com.date}
-                                            </p>
+                                        <p className={styles.commentText}>
+                                            {com.text}
+                                        </p>
+                                        <div className={styles.commentOptions}>
+                                            <div
+                                                className={
+                                                    styles.buttonsSection
+                                                }
+                                            >
+                                                <button>
+                                                    <i className="fa-solid fa-heart"></i>
+                                                    <p>{com.likes}</p>
+                                                </button>
+                                                <button>
+                                                    <i className="fa-solid fa-message"></i>
+                                                    <p>{com.comments}</p>
+                                                </button>
+                                                <button>
+                                                    <i className="fa-solid fa-share"></i>
+                                                    <p>Repost</p>
+                                                </button>
+                                            </div>
+                                            <button
+                                                className={styles.reportButton}
+                                            >
+                                                Report
+                                            </button>
                                         </div>
+                                        <hr />
                                     </div>
-                                    <p className={styles.commentText}>
-                                        {com.text}
-                                    </p>
-                                    <div className={styles.commentOptions}>
-                                        <div className={styles.buttonsSection}>
-                                            <button>
-                                                <i className="fa-solid fa-heart"></i>
-                                                <p>{com.likes}</p>
-                                            </button>
-                                            <button>
-                                                <i className="fa-solid fa-message"></i>
-                                                <p>{com.comments}</p>
-                                            </button>
-                                            <button>
-                                                <i className="fa-solid fa-share"></i>
-                                                <p>Repost</p>
-                                            </button>
-                                        </div>
-                                        <button className={styles.reportButton}>
-                                            Report
-                                        </button>
-                                    </div>
-                                    <hr />
-                                </div>
-                            ))}
+                                ))
+                            ) : (
+                                <p className={styles.noComments}>No one has commented on this property yet.</p>
+                            )}
                         </div>
                     </div>
                 ))}
